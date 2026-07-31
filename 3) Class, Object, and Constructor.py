9c7a9a90531b41bd3bcc382d627c9cf554822073
shravanigaikwad459-gif <shravanigaikwad459@gmail.com>
2026-08-01 00:39:28 +0530

    Add files via upload

---
+++ 3) Class, Object, and Constructor.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self):
+        self.name = "shravani"
+        self.college = "MIT ADT"
+
+    def display(self):
+        print(self.name)
+        print(self.college)
+
+s = Student()
+s.display()
