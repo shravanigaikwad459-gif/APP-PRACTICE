2baec0360001d62b64157ab6b280ffd556c7a66c
shravanigaikwad459-gif <shravanigaikwad459@gmail.com>
2026-08-01 00:42:30 +0530

    Add files via upload

---
+++ 4) Inheritance.py
@@ -0,0 +1,11 @@
+class Student:
+    def show(self):
+        print("Name: shravani")
+
+class College(Student):
+    def display(self):
+        print("College: MIT ADT")
+
+c = College()
+c.show()
+c.display()
